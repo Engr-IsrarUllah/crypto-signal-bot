61ed438ea42595c1fb4bfeb66e514bf35ab140f2
Engr-IsrarUllah <engrisrar256@gmail.com>
2026-05-06 11:27:42 +0500

    chore: remove unused dependency, add start script, ignore node_modules and .env

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ Time: ${new Date().toLocaleString()}
   res.send("Signal sent");
 });
 
-// app.listen(3000, () => {
-//   console.log("🚀 Server running on port 3000");
-// });
-
-module.exports = app;
+app.listen(3000, () => {
+  console.log("🚀 Server running on port 3000");
+});
